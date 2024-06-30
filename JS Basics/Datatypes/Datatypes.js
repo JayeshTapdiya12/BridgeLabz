3d3fe33b786f541e28e9d414613a961c722598e4
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-06-30 21:56:50 +0530

    28 june 2024 day  work

---
+++ JS Basics/Datatypes/Datatypes.js
@@ -0,0 +1,25 @@
+// Numbers:
+let length = 12;
+let weight = 10;
+console.log(length)
+console.log(weight)
+
+// Strings:
+let color = "Blue";
+let lastName = "Jayesh";
+
+console.log(color)
+console.log(lastName)
+// Booleans
+let x = true;
+let y = false;
+console.log(x)
+console.log(y)
+// Object:
+const person = { firstName: "Jayesh", lastName: "Tapdiya" };
+
+console.log(person)
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+console.log(cars)
---
+++ JS Basics/Variables/variables.js
@@ -0,0 +1,28 @@
+
+
+// Eg of var
+var message;
+
+message = 'Hello';
+
+console.log(message)
+
+// Eg of Let
+
+let x = 3;
+{
+    let x = 3;
+    console.log(x)
+}
+
+{
+    let x = 2;
+    console.log(x)
+}
+console.log(x)
+
+
+// Eg of Const 
+const birthday = "12.12.2002"
+birthday = "12.12.2003"
+console.log(birthday)
