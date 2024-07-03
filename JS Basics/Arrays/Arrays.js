054c7eea7511addedacb0fe2fe8a1ee3025152e5
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-07-03 11:00:52 +0530

    added array content

---
+++ JS Basics/Arrays/Arrays.js
@@ -0,0 +1,40 @@
+let my = ["jayesh", "tapdiya", "3rdyr", "7sem"];
+
+// length of array
+
+// console.log(my.length)
+
+// push new item
+// my.push("medicaps")
+// console.log(my)
+
+//
+// let pop = my.pop()
+// console.log(pop)
+// console.log(my)
+
+// to string
+
+// console.log(my.toString())
+
+// to join
+// console.log(my.join(3))
+
+// concat
+let my2 = ["123", "345"];
+// console.log(my.concat(my2))
+
+//  spred  operarotr
+
+let my3 = [...my, ...my2]
+// console.log(my3)
+
+// isme problem hia
+// spliec
+// console.log(my.splice(2, 0, "123", "456"));
+// console.log(my)
+
+// slice
+console.log(my.splice(0, 1));
+
+
